ef302ed2ff73403ff07024def0477989c7bcc481
shawn <shawn24601@gmail.com>
2018-05-28 14:48:18 +0800

    new

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,31 @@ import pandas as pd
 from sklearn.tree import export_graphviz
 
 
+def read_vcf(vcf_file):
+    buffer = []
+    header = ""
+    with open(vcf_file, 'r') as f:
+        for l in f:
+            l = l.rstrip()
+            if l.startswith("##"):
+                continue
+            elif l.startswith("#"):
+                l = l[1:]
+                header = l
+            else:
+                buffer.append(l)
+    vcf_df = pd.DataFrame.from_records([xx.split("\t") for xx in buffer], columns=header.split("\t"))
+    return vcf_df
+
+
+def annotation_vs_standard(vcf_df, gene_56_df):
+
+    joined_df = gene_56_df.merge(vcf_df, how='inner',
+                     on=['chr_num', 'position'])
+
+    return joined_df[['label', 'INFO']]
+
+
 def eval_clf(y_predicted, identifier_df, gene_56_df):
     y_pred_df = pd.DataFrame(y_predicted, columns=['predict'])
     # gene_56_df.merge(compare_df.join(identifier_df), how='left',
@@ -16,6 +41,7 @@ def eval_clf(y_predicted, identifier_df, gene_56_df):
     print("test accuracy 2: {}".format(predict_sum / df_size))
 
     print("Done")
+
 
 def plot_tree(tree_clf, X_train):
     # Plot tree
